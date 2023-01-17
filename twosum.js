3cd19a9ad742d71cd725c7d571c70815573db89f
Hamid <25hamid04@gmail.com>
2023-01-17 13:59:32 +0100

    comment

--- twosum.js
+++ twosum.js
@@ -4,6 +4,8 @@
  * @return {number[]}
  */
 
+// this code below is my solution of the task Twosum
+
 function twoSum (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
